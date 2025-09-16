d1e3691fbdf7dd66c7bdeacdcc52cf6fe8e3b20d
Vismay Gamit <42162523+vismaygamit@users.noreply.github.com>
2025-09-16 22:08:55 +0530

    update version

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -16,7 +16,7 @@ export const checkout = async (req, res) => {
 
     // Initialize Stripe with secret key
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: "2022-11-15",
+      apiVersion: "2025-06-30.basil",
     });
 
 
